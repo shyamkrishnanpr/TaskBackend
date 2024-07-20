d33435c7ff4b10ac2c5f84c2878ae2c53668a5c0
shyamkrishnanpr <122676350+shyamkrishnanpr@users.noreply.github.com>
2024-07-20 19:57:09 +0530

    task management apis created

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ import mongoose from "mongoose";
 import dotenv from "dotenv";
 import connectDB from "./config/db.js";
 import authRoutes from "./routes/authRoutes.js";
+import taskRoutes from "./routes/taskRoutes.js";
 
 dotenv.config();
 
@@ -13,6 +14,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.use("/api/auth", authRoutes);
+app.use("/api/task", taskRoutes);
 
 const PORT = process.env.PORT || 5000;
 
