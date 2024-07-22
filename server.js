b947202fe2feb026395cb3721bbf51186736aaf9
shyamkrishnanpr <122676350+shyamkrishnanpr@users.noreply.github.com>
2024-07-22 22:47:34 +0530

    cors updated

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(
   cors({
     credentials: true,
-    origin: ["http://localhost:5173", "*"],
+    origin: ["http://localhost:5173", "https://taskmanagervoosh.netlify.app"],
     methods: ["GET,HEAD,PUT,PATCH,POST,DELETE"],
   })
 );
